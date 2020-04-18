7ba445d40cdef119fc060d82e972c145b4bb809e
chelseasimek <chelsea.simek@temple.edu>
2020-04-18 13:58:54 -0400

    add in fieldsets

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Provider } from 'react-redux';
-import store from './store';
+import { createStore } from 'redux'
+// import todoApp from './reducers'
 
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// const store = createStore(todoApp)
+
 ReactDOM.render(
-  <Provider store={store}>
     <React.StrictMode>
       <App />
-    </React.StrictMode>
-  </Provider>,
+    </React.StrictMode>,
   document.getElementById('root')
 );
 
--- src/pages/search/SearchPage.js
+++ src/pages/search/SearchPage.js
@@ -20,74 +20,80 @@ export default function Search(){
       <label for="search-id">Search</label>
       <input type="text" id="search-id" name="search-id"></input>
       <h2>Colors</h2>
-      <RadioGroup { ...colorRadio } aria-label="shirt-colors">
-        <label>
-          <Radio { ...colorRadio } value="red" /> Red
-        </label>
-        <label>
-          <Radio { ...colorRadio } value="orange" /> Orange
-        </label>
-        <label>
-          <Radio { ...colorRadio } value="yellow" /> Yellow
-        </label>
-        <label>
-          <Radio { ...colorRadio } value="green" /> Green
-        </label>
-        <label>
-          <Radio { ...colorRadio } value="blue" /> Blue
-        </label>
-        <label>
-          <Radio { ...colorRadio } value="purple" /> Purple
-        </label>
-      </RadioGroup>
+      <fieldset>   
+        <RadioGroup { ...colorRadio } aria-label="shirt-colors">
+          <label>
+            <Radio { ...colorRadio } value="red" /> Red
+          </label>
+          <label>
+            <Radio { ...colorRadio } value="orange" /> Orange
+          </label>
+          <label>
+            <Radio { ...colorRadio } value="yellow" /> Yellow
+          </label>
+          <label>
+            <Radio { ...colorRadio } value="green" /> Green
+          </label>
+          <label>
+            <Radio { ...colorRadio } value="blue" /> Blue
+          </label>
+          <label>
+            <Radio { ...colorRadio } value="purple" /> Purple
+          </label>
+        </RadioGroup>
+      </fieldset>   
 
       <h2>Size</h2>
-      <label>
-        <Checkbox {...sizeCheckbox} value="xs" />
-        X-Small
-      </label>
-      <label>
-        <Checkbox {...sizeCheckbox} value="s" />
-        Small
-      </label>
-      <label>
-        <Checkbox {...sizeCheckbox} value="m" />
-        Medium
-      </label>
-      <label>
-        <Checkbox {...sizeCheckbox} value="l" />
-        Large
-      </label>
-      <label>
-        <Checkbox {...sizeCheckbox} value="xl" />
-        X-Large
-      </label>
+      <fieldset>   
+        <label>   
+          <Checkbox {...sizeCheckbox} value="xs" />
+          X-Small
+        </label>
+        <label>
+          <Checkbox {...sizeCheckbox} value="s" />
+          Small
+        </label>
+        <label>
+          <Checkbox {...sizeCheckbox} value="m" />
+          Medium
+        </label>
+        <label>
+          <Checkbox {...sizeCheckbox} value="l" />
+          Large
+        </label>
+        <label>
+          <Checkbox {...sizeCheckbox} value="xl" />
+          X-Large
+        </label>
+      </fieldset>    
 
       <h2>Occasion</h2>
-      <label>
-        <Checkbox {...sizeCheckbox} value="formal" />
-        Formal
-      </label>
-      <label>
-        <Checkbox {...sizeCheckbox} value="casual" />
-        Casual
-      </label>
-      <label>
-        <Checkbox {...sizeCheckbox} value="coctail" />
-        Cocktail
-      </label>
-      <label>
-        <Checkbox {...sizeCheckbox} value="back-tie" />
-        Black Tie
-      </label>
-      <label>
-        <Checkbox {...sizeCheckbox} value="sport" />
-        Sport
-      </label>
-      <label>
-        <Checkbox {...sizeCheckbox} value="business" />
-        Business
-      </label>
+      <fieldset>   
+        <label>
+          <Checkbox {...sizeCheckbox} value="formal" />
+          Formal
+        </label>
+        <label>
+          <Checkbox {...sizeCheckbox} value="casual" />
+          Casual
+        </label>
+        <label>
+          <Checkbox {...sizeCheckbox} value="coctail" />
+          Cocktail
+        </label>
+        <label>
+          <Checkbox {...sizeCheckbox} value="back-tie" />
+          Black Tie
+        </label>
+        <label>
+          <Checkbox {...sizeCheckbox} value="sport" />
+          Sport
+        </label>
+        <label>
+          <Checkbox {...sizeCheckbox} value="business" />
+          Business
+        </label>
+      </fieldset>    
     </React.Fragment>
   )
 }
