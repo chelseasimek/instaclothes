f6f5e4d8a0d22273b489082891c6ab997b24ccee
chelseasimek <chelsea.z.simek@gmail.com>
2020-04-18 13:43:52 -0400

    add search form fields'

--- src/pages/search/SearchPage.js
+++ src/pages/search/SearchPage.js
@@ -1,15 +1,23 @@
 import React from 'react';
 
-  import { useRadioState, Radio, RadioGroup } from "reakit/Radio";
+  import { 
+    useRadioState,
+    Radio,
+    RadioGroup,
+    useCheckboxState,
+    Checkbox
+   } from "reakit";
 
 export default function Search(){
 
  const colorRadio = useRadioState({ state: 'red' });
+ const sizeCheckbox = useCheckboxState({ state: [] });
   return (
     <React.Fragment>
       <h1> SEARCH </h1>
       <label for="search-id">Search</label>
       <input type="text" id="search-id" name="search-id"></input>
+      <h2>Colors</h2>
       <RadioGroup { ...colorRadio } aria-label="shirt-colors">
         <label>
           <Radio { ...colorRadio } value="red" /> red
@@ -30,6 +38,54 @@ export default function Search(){
           <Radio { ...colorRadio } value="purple" /> purple
         </label>
       </RadioGroup>
+
+      <h2>Size</h2>
+      <label>
+        <Checkbox {...sizeCheckbox} value="xs" />
+        X-Small
+      </label>
+      <label>
+        <Checkbox {...sizeCheckbox} value="s" />
+        Small
+      </label>
+      <label>
+        <Checkbox {...sizeCheckbox} value="m" />
+        Medium
+      </label>
+      <label>
+        <Checkbox {...sizeCheckbox} value="l" />
+        Large
+      </label>
+      <label>
+        <Checkbox {...sizeCheckbox} value="xl" />
+        X-Large
+      </label>
+
+      <h2>Occasion</h2>
+      <label>
+        <Checkbox {...sizeCheckbox} value="formal" />
+        Formal
+      </label>
+      <label>
+        <Checkbox {...sizeCheckbox} value="casual" />
+        Casual
+      </label>
+      <label>
+        <Checkbox {...sizeCheckbox} value="coctail" />
+        Cocktail
+      </label>
+      <label>
+        <Checkbox {...sizeCheckbox} value="back-tie" />
+        Black Tie
+      </label>
+      <label>
+        <Checkbox {...sizeCheckbox} value="sport" />
+        Sport
+      </label>
+      <label>
+        <Checkbox {...sizeCheckbox} value="business" />
+        Business
+      </label>
     </React.Fragment>
   )
 }
