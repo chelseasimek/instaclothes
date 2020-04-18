c6c1f43c8b66ece5e16ad52f8e3a210c1e3a6e7a
chelsea simek <chelseasimek@chelseas-MacBook-Pro.local>
2020-04-18 12:55:45 -0400

    add router routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import React from 'react';
 import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/search">
+
+          </Route>
+          <Route path="/results">
+
+          </Route>
+          <Route path="/details/:itemName">
+
+          </Route>
+          <Route path="/search">
+
+          </Route>
+          <Route path="/quote">
+
+          </Route>
+          <Route path="/confirmation">
+
+          </Route>
+          <Route path="/delivery/:type">
+
+          </Route>
+          <Route path="/rating">
+
+          </Route>
+      </Switch>
+      </div>
+    </Router>
     </div>
   );
 }
