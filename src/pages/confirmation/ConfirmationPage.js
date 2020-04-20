8da8df5106e7559ba94c676252783d983b6f8df2
chelseasimek <chelsea.simek@temple.edu>
2020-04-19 21:16:25 -0400

    confirmation page

--- src/pages/confirmation/ConfirmationPage.js
+++ src/pages/confirmation/ConfirmationPage.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
 import { 
-    Button, Box
+    Button,
+    Box,
+    useDialogState,
+    Dialog,
+    DialogDisclosure,
+    DialogBackdrop,
 } from "reakit";
+import { swapPage } from '../../utils';
 
 export default function Confirmation(){
+    const dialog = useDialogState();
+
+    let _cancelOrder = () => {
+        dialog.hide();
+        swapPage('search');
+    }
+
+    let _viewOrder = () => {
+        swapPage('checkout');
+    }
     return (
         <React.Fragment>
             <Box tabIndex={0}>
@@ -13,8 +29,17 @@ export default function Confirmation(){
                 <p>Your driver is enroute with your order from the store.</p>
                 <p>Your estimated wait time is now 30 minutes.</p>
             </Box>
-            <Button>View Order</Button>
-            <Button>Cancel Order</Button>
+            <Button onClick={_viewOrder}>View Order</Button>
+            <>
+                <DialogDisclosure {...dialog}>Cancle Order</DialogDisclosure>
+                <DialogBackdrop {...dialog}>
+                    <Dialog {...dialog} tabIndex={0} aria-label="Cancel Order">
+                        <h2>Cancel Order</h2>
+                        <p>Cancelling before your shopper has left for the store will not incure any fees</p>
+                        <Button onClick={_cancelOrder}>Cancel Order</Button>
+                    </Dialog>
+                </DialogBackdrop>
+            </>
         </React.Fragment>
     )
 }
