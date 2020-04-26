7bccd8fff19d96c2bed8be1da434a2be8dc7f6d1
csimek697 <chelsea_simek@comcast.com>
2020-04-26 00:22:03 -0400

    Updates

---
+++ precache-manifest.144a3a45ac9dfb64a85a0d5961d13883.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bc38470b6fa187c1bbeb48e5308b356",
+    "url": "/instaclothes/index.html"
+  },
+  {
+    "revision": "7713df017fcba4b368b9",
+    "url": "/instaclothes/static/css/main.266173d9.chunk.css"
+  },
+  {
+    "revision": "10b8d41ac8a732329de3",
+    "url": "/instaclothes/static/js/2.1a4d399d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/instaclothes/static/js/2.1a4d399d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7713df017fcba4b368b9",
+    "url": "/instaclothes/static/js/main.622eaf3a.chunk.js"
+  },
+  {
+    "revision": "b7b696bae6d96faf08bc",
+    "url": "/instaclothes/static/js/runtime-main.6b7a3714.js"
+  }
+]);
